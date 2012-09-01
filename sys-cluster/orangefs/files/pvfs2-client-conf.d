bd51f420ebf0b7ff27f81a7101b844742ff98ce8
Andrew Savchenko <bircoph@gmail.com>
2012-09-01 16:12:02 +0400

    orangefs: fix ordering of options in client's conf

--- sys-cluster/orangefs/files/pvfs2-client-conf.d
+++ sys-cluster/orangefs/files/pvfs2-client-conf.d
@@ -19,17 +19,6 @@
 # Entries with noauto option are ignored.
 #PVFS2_CLIENT_FSTAB="/etc/fstab"
 
-### Options for kernel client only ###
-
-# Extra arguments to supply to the pvfs2-client daemon
-#PVFS2_CLIENT_LOG="/var/log/pvfs2/client.log"
-
-# Extra arguments to supply to the pvfs2-client daemon
-#PVFS2_CLIENT_ARGS=""
-
-# Allows to unload pvfs2 kernel module on stop
-#PVFS2_CLIENT_UNLOAD_MODULE="yes"
-
 # Determines how many times to run pvfs2-ping check before giving
 # up on a mount point. Single pvfs2-ping run takes about 10 seconds
 # if server is unavailable.
@@ -51,6 +40,17 @@
 
 # Nice priority of the client
 #PVFS2_CLIENT_NICE=""
+
+### Options for kernel client only ###
+
+# Extra arguments to supply to the pvfs2-client daemon
+#PVFS2_CLIENT_LOG="/var/log/pvfs2/client.log"
+
+# Extra arguments to supply to the pvfs2-client daemon
+#PVFS2_CLIENT_ARGS=""
+
+# Allows to unload pvfs2 kernel module on stop
+#PVFS2_CLIENT_UNLOAD_MODULE="yes"
 
 ### Options for fuse client only ###
 
